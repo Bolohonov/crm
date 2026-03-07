20be584d74f963ae39fd3a28888f333ff7600fa4
bolohonov <bolohonov@list.ru>
2026-03-07 13:21:17 +0400

    fix kafka

--- crm-backend/src/main/java/com/crm/CrmApplication.java
+++ crm-backend/src/main/java/com/crm/CrmApplication.java
@@ -3,11 +3,13 @@ package com.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan   // сканирует все @ConfigurationProperties
 @EnableScheduling              // для очистки устаревших токенов по расписанию
+@EnableAsync
 public class CrmApplication {
 
     public static void main(String[] args) {
