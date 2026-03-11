350ad667a2b0e87ad96296afe4a640e8df174b6f
bolohonov <bolohonov@list.ru>
2026-03-11 20:03:02 +0400

    fix modal

--- crm-frontend/src/main.ts
+++ crm-frontend/src/main.ts
@@ -40,6 +40,19 @@ app.use(PrimeVue, {
   theme: {
     preset: Aura,
     options: { prefix: 'p', darkModeSelector: '[data-theme="dark"]', cssLayer: false }
+  },
+  locale: {
+    accept: 'Да',
+    reject: 'Нет',
+    cancel: 'Отмена',
+    dayNames: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+    dayNamesShort: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+    today: 'Сегодня',
+    clear: 'Очистить',
+    firstDayOfWeek: 1,
   }
 })
 app.component('DataTable', DataTable)
